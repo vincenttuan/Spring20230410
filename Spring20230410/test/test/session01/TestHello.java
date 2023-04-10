6f2f1302bb560f81c8e2fbfff093c2ce9d39bad2
teacher <teacher@BE-403->
2023-04-10 20:03:03 +0800

    spring

--- Spring20230410/test/test/session01/TestHello.java
+++ Spring20230410/test/test/session01/TestHello.java
@@ -1,12 +1,22 @@
 package test.session01;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.spring.core.session01.bean.Hello;
 
 public class TestHello {
 
 	public static void main(String[] args) {
+		// 傳統用法
 		Hello hello = new Hello();
 		System.out.println(hello);
+		
+		// Spring 用法(透過 beans-config.xml 得到 Hello 的物件實體)
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config.xml");
+		Hello hello2 = (Hello)ctx.getBean("hello");
+		System.out.println(hello2);
+		
 	}
 
 }
