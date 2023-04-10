ecfd637be3f8ee360cb9439b6999663ec109592d
teacher <teacher@BE-403->
2023-04-10 19:49:33 +0800

    spring

---
+++ Spring20230410/test/test/session01/TestHello.java
@@ -0,0 +1,12 @@
+package test.session01;
+
+import com.spring.core.session01.bean.Hello;
+
+public class TestHello {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		System.out.println(hello);
+	}
+
+}
