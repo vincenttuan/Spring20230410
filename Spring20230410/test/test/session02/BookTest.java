d14d4dbb4dd4baa7d85f224fcd7463aa2d5f774f
teacher <teacher@BE-403->
2023-04-14 21:30:04 +0800

    spring

--- Spring20230410/test/test/session02/BookTest.java
+++ Spring20230410/test/test/session02/BookTest.java
@@ -28,20 +28,26 @@ public class BookTest {
 		Book book3 = ctx.getBean("book3", Book.class);
 		Book book4 = ctx.getBean("book4", Book.class);
 		Book book5 = ctx.getBean("book5", Book.class);
+		Book book6 = ctx.getBean("book6", Book.class);
+		Book book7 = ctx.getBean("book7", Book.class);
+		Book book8 = ctx.getBean("book8", Book.class);
 		System.out.println(book2);
 		System.out.println(book3);
 		System.out.println(book4);
 		System.out.println(book5);
+		System.out.println(book6);
+		System.out.println(book7);
+		System.out.println(book8);
 		
 		// 請問 book1~book5 書籍總價 = ?
-		int sum = List.of(book1, book2, book3, book4, book5)
+		int sum = List.of(book1, book2, book3, book4, book5, book6, book7, book8)
 					  .stream()
 					  .mapToInt(Book::getPrice)
 					  .sum();
 		System.out.println(sum);
 		
 		// 請問所有 book1 ~ book5 作者的平均年齡 = ?
-		double avg = List.of(book1, book2, book3, book4, book5)
+		double avg = List.of(book1, book2, book3, book4, book5, book6, book7, book8)
 				  		 .stream()
 				  		 .mapToInt(b -> b.getAuthor().getAge())
 				  		 .average()
