7bfac0dea2c2d2b37f4a047607274ab5a175cdfb
teacher <teacher@BE-403->
2023-04-17 21:14:22 +0800

    spring

--- Spring20230410/test/test/session03/ClazzTest.java
+++ Spring20230410/test/test/session03/ClazzTest.java
@@ -4,6 +4,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.spring.core.session03.bean.Clazz;
+import com.spring.core.session03.bean.Student;
+import com.spring.core.session03.bean.Teacher;
 
 public class ClazzTest {
 
@@ -15,6 +17,14 @@ public class ClazzTest {
 		System.out.println(clazz2);
 		Clazz clazz3 = ctx.getBean("clazz3", Clazz.class);
 		System.out.println(clazz3);
+		
+		Student student1 = ctx.getBean("student1", Student.class);
+		Student student2 = ctx.getBean("student2", Student.class);
+		System.out.println(student1);
+		System.out.println(student2);
+		
+		Teacher teacher1 = ctx.getBean("teacher1", Teacher.class);
+		System.out.println(teacher1);
 		
 		
 	}
