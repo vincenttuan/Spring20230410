b3360be873fe5752a0fe26676547407d63dce042
teacher <teacher@BE-403->
2023-04-17 18:54:10 +0800

    spring

---
+++ Spring20230410/src/com/spring/core/session02/bean/Paper.java
@@ -0,0 +1,51 @@
+package com.spring.core.session02.bean;
+
+public class Paper {
+	private Integer id; // 紙張編號 
+	private Color color; // 紙張顏色
+	private Size size; // 紙張尺寸
+	
+	public Paper() {
+	
+	}
+
+	public Paper(Color color, Size size) {
+		this.color = color;
+		this.size = size;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public Size getSize() {
+		return size;
+	}
+
+	public void setSize(Size size) {
+		this.size = size;
+	}
+
+	@Override
+	public String toString() {
+		return "Paper [id=" + id + ", color=" + color + ", size=" + size + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ Spring20230410/src/com/spring/core/session02/bean/Size.java
@@ -0,0 +1,20 @@
+package com.spring.core.session02.bean;
+
+public class Size {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Size [name=" + name + "]";
+	}
+	
+	
+}
