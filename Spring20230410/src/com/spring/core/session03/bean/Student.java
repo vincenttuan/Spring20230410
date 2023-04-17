ccd3da2b41406b77557d74a09300ff894b7dbdba
teacher <teacher@BE-403->
2023-04-17 20:56:43 +0800

    spring

---
+++ Spring20230410/src/com/spring/core/session03/bean/Student.java
@@ -0,0 +1,36 @@
+package com.spring.core.session03.bean;
+
+import java.util.Set;
+
+public class Student {
+	private Integer id; // 學號
+	private String name; // 姓名
+	private Set<Clazz> clazzs; // 選課
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Set<Clazz> getClazzs() {
+		return clazzs;
+	}
+	public void setClazzs(Set<Clazz> clazzs) {
+		this.clazzs = clazzs;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", clazzs=" + clazzs + "]";
+	}
+	
+	
+	
+	
+}
---
+++ Spring20230410/src/com/spring/core/session03/bean/Teacher.java
@@ -0,0 +1,52 @@
+package com.spring.core.session03.bean;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Teacher {
+	private Integer id; // 編號
+	private String name; // 姓名
+	private Set<Student> students; // 學生
+	private List<Clazz> clazzs; // 授課科目
+	private Map<String, Integer> salary; // 薪資組合
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Set<Student> getStudents() {
+		return students;
+	}
+	public void setStudents(Set<Student> students) {
+		this.students = students;
+	}
+	public List<Clazz> getClazzs() {
+		return clazzs;
+	}
+	public void setClazzs(List<Clazz> clazzs) {
+		this.clazzs = clazzs;
+	}
+	public Map<String, Integer> getSalary() {
+		return salary;
+	}
+	public void setSalary(Map<String, Integer> salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Teacher [id=" + id + ", name=" + name + ", students=" + students + ", clazzs=" + clazzs + ", salary="
+				+ salary + "]";
+	}
+	
+	
+	
+}
