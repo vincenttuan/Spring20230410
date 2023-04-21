c141fe0ae220e0b347e44930601df9d9d9fc9b96
teacher <teacher@BE-403->
2023-04-21 21:35:58 +0800

    spring

--- Spring20230410/src/com/spring/core/session03/mvc/service/UserService.java
+++ Spring20230410/src/com/spring/core/session03/mvc/service/UserService.java
@@ -1,5 +1,7 @@
 package com.spring.core.session03.mvc.service;
 
-public interface UserService {
+import com.spring.core.session03.mvc.model.User;
 
+public interface UserService {
+	public void addUser(User user);
 }
