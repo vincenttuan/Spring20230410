a7f08e992a4e7f7d861a6f8066e9e0d04f4fdb4c
teacher <teacher@BE-403->
2023-04-21 21:44:57 +0800

    spring

--- Spring20230410/src/com/spring/core/session03/mvc/controller/UserController.java
+++ Spring20230410/src/com/spring/core/session03/mvc/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.spring.core.session03.mvc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import com.spring.core.session03.mvc.model.User;
 import com.spring.core.session03.mvc.service.UserService;
 
+@Controller
 public class UserController {
 	
 	@Autowired
