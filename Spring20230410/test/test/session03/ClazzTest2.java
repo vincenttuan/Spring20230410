d07bfca8563ba6a2a3c9b27a061fc33f3995b15f
teacher <teacher@BE-403->
2023-04-21 19:54:18 +0800

    spring

--- Spring20230410/test/test/session03/ClazzTest2.java
+++ Spring20230410/test/test/session03/ClazzTest2.java
@@ -1,10 +1,21 @@
 package test.session03;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.core.session03.SpringConfig3;
+import com.spring.core.session03.bean.Clazz;
+
 public class ClazzTest2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config3.xml"); // xml 配置
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig3.class); // Java 配置
+		
+		Clazz clazz1 = ctx.getBean("clazz1", Clazz.class);
+		System.out.println(clazz1);
+		
 	}
 
 }
