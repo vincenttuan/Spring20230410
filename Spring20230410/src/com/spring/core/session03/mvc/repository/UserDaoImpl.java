7f744b798627c9fc2afeb6510aaeff492859de27
teacher <teacher@BE-403->
2023-04-21 21:35:10 +0800

    spring

---
+++ Spring20230410/src/com/spring/core/session03/mvc/repository/UserDaoImpl.java
@@ -0,0 +1,15 @@
+package com.spring.core.session03.mvc.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.spring.core.session03.mvc.model.User;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public void createUser(User user) {
+		System.out.println("UserDao create user ok: " + user);
+	}
+	
+}
