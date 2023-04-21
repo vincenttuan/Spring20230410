cfecd91014b5c436a34737297d88ed04a3664211
teacher <teacher@BE-403->
2023-04-21 20:53:07 +0800

    spring

--- Spring20230410/src/com/spring/core/session03/mvc/model/User.java
+++ Spring20230410/src/com/spring/core/session03/mvc/model/User.java
@@ -1,5 +1,13 @@
 package com.spring.core.session03.mvc.model;
 
-public class User {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
+@Component
+@PropertySource("classpath:user.properties")
+public class User {
+	//@Value(value = "Mary")
+	@Value("${user.username}")
+	private String username;
 }
