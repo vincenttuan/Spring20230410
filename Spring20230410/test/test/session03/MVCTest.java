a3e268c4e9c05eaec75ce67739ef579baff936a4
teacher <teacher@BE-403->
2023-04-21 21:24:41 +0800

    spring

---
+++ Spring20230410/test/test/session03/MVCTest.java
@@ -0,0 +1,16 @@
+package test.session03;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.core.session03.mvc.model.User;
+
+public class MVCTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config3-mvc.xml");
+		User user = ctx.getBean("user",  User.class);
+		System.out.println(user);
+	}
+
+}
