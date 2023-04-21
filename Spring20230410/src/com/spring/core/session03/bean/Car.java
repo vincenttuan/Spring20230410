10b1a0b0c591e590290bb6b23a4a7e0420fc6e0c
teacher <teacher@BE-403->
2023-04-21 19:13:33 +0800

    spring

---
+++ Spring20230410/src/com/spring/core/session03/bean/Car.java
@@ -0,0 +1,37 @@
+package com.spring.core.session03.bean;
+
+public class Car {
+	private String name;
+	private Integer price;
+	
+	public Car() {
+		
+	}
+	
+	public Car(String name, Integer price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [name=" + name + ", price=" + price + "]";
+	}
+	
+}
