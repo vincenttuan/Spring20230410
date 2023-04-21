ec9120c70afdb58b9210996eccc86f1fb6e905ca
teacher <teacher@BE-403->
2023-04-21 19:29:36 +0800

    spring

---
+++ Spring20230410/src/com/spring/core/session03/bean/CarFactory.java
@@ -0,0 +1,27 @@
+package com.spring.core.session03.bean;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.FactoryBean;
+
+public class CarFactory implements FactoryBean<Car> {
+
+	@Override
+	public Car getObject() throws Exception {
+		Random random = new Random();
+		int price = random.nextInt(300_0000);
+		 Car car = new Car("BMW", price);
+		return car;
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Car.class;
+	}
+
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
+
+}
