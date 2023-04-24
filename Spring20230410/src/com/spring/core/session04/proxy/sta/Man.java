12c8a73b6afd86856fef9ba664e2a178d2d827ac
teacher <teacher@BE-403->
2023-04-24 19:15:08 +0800

    spring

--- Spring20230410/src/com/spring/core/session04/proxy/sta/Man.java
+++ Spring20230410/src/com/spring/core/session04/proxy/sta/Man.java
@@ -4,9 +4,14 @@ public class Man implements Person {
 
 	@Override
 	public void work() {
+		// 公用邏輯
+		System.out.println("出門戴口罩");
 		
 		// 業務邏輯
 		System.out.println("上班工作");
+		
+		// 公用邏輯
+		System.out.println("回家脫口罩");
 		
 	}
 
