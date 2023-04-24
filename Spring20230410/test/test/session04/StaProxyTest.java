330b26da9eceaf2965b7778a3a6ea13425e9f7f0
teacher <teacher@BE-403->
2023-04-24 19:25:09 +0800

    spring

--- Spring20230410/test/test/session04/StaProxyTest.java
+++ Spring20230410/test/test/session04/StaProxyTest.java
@@ -2,17 +2,21 @@ package test.session04;
 
 import com.spring.core.session04.proxy.sta.Man;
 import com.spring.core.session04.proxy.sta.Person;
+import com.spring.core.session04.proxy.sta.PersonProxy;
 import com.spring.core.session04.proxy.sta.Woman;
 
 public class StaProxyTest {
 
 	public static void main(String[] args) {
 		// 不透過代理直接執行
-		Person man = new Man();
-		Person woman = new Woman();
+		//Person man = new Man();
+		//Person woman = new Woman();
+		
+		// 透過代理物件建構
+		Person man = new PersonProxy(new Man());
+		Person woman = new PersonProxy(new Woman());
 		man.work();
 		woman.work();
-
 	}
 
 }
