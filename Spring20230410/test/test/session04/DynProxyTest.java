489673577684ab9c5f70637c6d6d2fbb0a350fec
teacher <teacher@BE-403->
2023-04-24 20:57:10 +0800

    spring

--- Spring20230410/test/test/session04/DynProxyTest.java
+++ Spring20230410/test/test/session04/DynProxyTest.java
@@ -3,6 +3,8 @@ package test.session04;
 import com.spring.core.session04.proxy.dyn.Calc;
 import com.spring.core.session04.proxy.dyn.CalcImpl;
 import com.spring.core.session04.proxy.dyn.DynProxy;
+import com.spring.core.session04.proxy.sta.Man;
+import com.spring.core.session04.proxy.sta.Person;
 
 public class DynProxyTest {
 
@@ -10,6 +12,9 @@ public class DynProxyTest {
 		// 使用動態代理
 		Calc calc = (Calc)new DynProxy(new CalcImpl()).getProxy();
 		System.out.println(calc.add(10, 20));
+		
+		Person man = (Person)new DynProxy(new Man()).getProxy();
+		man.work();
 	}
 
 }
