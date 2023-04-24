b6078edf1f91c79fd6e32ee8e74a2298db0828ac
teacher <teacher@BE-403->
2023-04-24 19:43:49 +0800

    spring

--- Spring20230410/src/com/spring/core/session04/proxy/sta/PersonProxy.java
+++ Spring20230410/src/com/spring/core/session04/proxy/sta/PersonProxy.java
@@ -13,19 +13,20 @@ public class PersonProxy implements Person {
 	
 	@Override
 	public void work() {
-		// 公用邏輯
+		// 公用邏輯:Before 前置通知
 		System.out.println("出門戴口罩");
 		// 執行被代理者的業務方法
 		try {
 			person.work();
 		} catch (Exception e) {
-			// 公用例外邏輯
+			// 公用例外邏輯:Exception 異常通知
 			System.out.println(e);
 			System.out.println("去買口罩");
 			System.out.println("將口罩戴回");
+		} finally {
+			// 公用邏輯:End 後置通知
+			System.out.println("回家脫口罩");
 		}
-		// 公用邏輯
-		System.out.println("回家脫口罩");
 	}
 	
 }
