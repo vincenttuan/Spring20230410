3ef41066fdb36d75eb40548881f8a9b81ca2492d
teacher <teacher@BE-403->
2023-04-24 19:05:41 +0800

    spring

---
+++ Spring20230410/src/com/spring/core/session04/proxy/sta/Woman.java
@@ -0,0 +1,13 @@
+package com.spring.core.session04.proxy.sta;
+
+public class Woman implements Person {
+
+	@Override
+	public void work() {
+		
+		// 業務邏輯
+		System.out.println("去市場買菜");
+		
+	}
+
+}
