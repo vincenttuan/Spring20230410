ef37d2eaa80709ff2694e2e2bd06f325be23b3ab
teacher <teacher@BE-403->
2023-04-28 19:17:36 +0800

    spring

--- Spring20230410/test/test/session04/HelloTest.java
+++ Spring20230410/test/test/session04/HelloTest.java
@@ -6,9 +6,14 @@ import com.spring.core.session04.proxy.enhancer.Hello;
 public class HelloTest {
 
 	public static void main(String[] args) {
+		// 一般執行(無代理)
 		//Hello hello = new Hello();
+		//System.out.println(hello.sayHello("John"));
+		
+		// 透過增強子類執行(增強子類 = 代理者)
 		Hello hello = new EnhancerHello();
 		System.out.println(hello.sayHello("John"));
+		
 		
 		
 	}
