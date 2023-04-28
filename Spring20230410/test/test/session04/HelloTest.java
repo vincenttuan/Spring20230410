2a40849d549d8a4b26cf10642aa4e8f0d6415863
teacher <teacher@BE-403->
2023-04-28 18:51:15 +0800

    spring

---
+++ Spring20230410/test/test/session04/HelloTest.java
@@ -0,0 +1,13 @@
+package test.session04;
+
+import com.spring.core.session04.proxy.echancer.Hello;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		System.out.println(hello.sayHello("John"));
+
+	}
+
+}
