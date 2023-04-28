64b545019928c93369a6f8ca2561cc76c2946f0d
teacher <teacher@BE-403->
2023-04-28 19:58:44 +0800

    spring

---
+++ Spring20230410/test/test/session05/AOPTest.java
@@ -0,0 +1,19 @@
+package test.session05;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.core.session05.aop.Calc;
+import com.spring.core.session05.aop.CalcImpl;
+
+public class AOPTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config5.xml");
+		Calc calc = ctx.getBean("calcImpl", CalcImpl.class);
+		System.out.println(calc.add(20, 10));
+		System.out.println(calc.div(50, 5));
+
+	}
+
+}
