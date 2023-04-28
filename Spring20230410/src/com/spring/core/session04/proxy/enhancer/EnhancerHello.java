743fecbf8f6c743b0d10e98127e299f02d69bdb9
teacher <teacher@BE-403->
2023-04-28 19:02:07 +0800

    spring

--- Spring20230410/src/com/spring/core/session04/proxy/enhancer/EnhancerHello.java
+++ Spring20230410/src/com/spring/core/session04/proxy/enhancer/EnhancerHello.java
@@ -1,5 +1,11 @@
 package com.spring.core.session04.proxy.enhancer;
 
-public class EnhancerHello {
+// 子類別 / 增強(Enhancer)
+public class EnhancerHello extends Hello {
 
+	@Override // 覆寫 / 攔截(Interceptor)
+	public String sayHello(String str) {
+		return super.sayHello(str) + " 請出示實聯簡訊證明 !";
+	}
+	
 }
