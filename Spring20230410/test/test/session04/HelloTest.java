d3f87a5a7d27ea7c60928aab78a40585b8aa6ef4
teacher <teacher@BE-403->
2023-04-28 19:23:13 +0800

    spring

--- Spring20230410/test/test/session04/HelloTest.java
+++ Spring20230410/test/test/session04/HelloTest.java
@@ -1,7 +1,10 @@
 package test.session04;
 
+import org.springframework.cglib.proxy.Enhancer;
+
 import com.spring.core.session04.proxy.enhancer.EnhancerHello;
 import com.spring.core.session04.proxy.enhancer.Hello;
+import com.spring.core.session04.proxy.enhancer.MyMethodInterceptor;
 
 public class HelloTest {
 
@@ -10,12 +13,20 @@ public class HelloTest {
 		//Hello hello = new Hello();
 		//System.out.println(hello.sayHello("John"));
 		
-		// 透過增強子類執行(增強子類 = 代理者)
-		Hello hello = new EnhancerHello();
+		// 透過增強子類執行(增強子類 = 代理者), 靜態模式
+		//Hello hello = new EnhancerHello();
+		//System.out.println(hello.sayHello("John"));
+		
+		// 透過 cglib 來實現動態代理
+		// 1. 建立增強器 (cglib 本身就有內建)
+		Enhancer enhancer = new Enhancer();
+		// 2. 要增強的對象
+		enhancer.setSuperclass(Hello.class);
+		// 3. 配置方法攔截器
+		enhancer.setCallback(new MyMethodInterceptor());
+		// 4. 透過 enhancer 來得到代理物件
+		Hello hello = (Hello)enhancer.create();
 		System.out.println(hello.sayHello("John"));
-		
-		
-		
 	}
 
 }
