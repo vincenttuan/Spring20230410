897f276e17e9a14ae0a13d07e6749280cc5ae35f
teacher <teacher@BE-403->
2023-04-28 19:07:57 +0800

    spring

--- Spring20230410/src/com/spring/core/session04/proxy/enhancer/EnhancerHello.java
+++ Spring20230410/src/com/spring/core/session04/proxy/enhancer/EnhancerHello.java
@@ -5,8 +5,9 @@ public class EnhancerHello extends Hello {
 
 	@Override // 覆寫 / 攔截(Interceptor)
 	public String sayHello(String str) {
+		// 公用方法
 		System.out.println("請出示實聯證明");
-		return super.sayHello(str);
+		return super.sayHello(str); // 調用父類別的商業方法
 	}
 	
 }
--- Spring20230410/src/com/spring/core/session04/proxy/enhancer/Hello.java
+++ Spring20230410/src/com/spring/core/session04/proxy/enhancer/Hello.java
@@ -1,6 +1,7 @@
 package com.spring.core.session04.proxy.enhancer;
 
 public class Hello {
+	// 商業方法
 	public String sayHello(String str) { // 方法簽章
 		return "Hello " + str;
 	}
