e6991656c446d3ff625284cd798ab84251833e30
teacher <teacher@BE-403->
2023-04-28 21:17:53 +0800

    spring

--- Spring20230410/src/com/spring/core/session05/aop/MyLoggerAspect.java
+++ Spring20230410/src/com/spring/core/session05/aop/MyLoggerAspect.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +14,23 @@ import org.springframework.stereotype.Component;
 @Order(1) // 數字越小越先被執行 (預設值是 int 的最大值)
 // Order 的使用情境是在有多個切面程式在同一個橫切關注點要執行時
 public class MyLoggerAspect {
+	// 建立一個切入點(Pointcut)方法
+	@Pointcut(value = "execution(* com.spring.core.session05.aop.CalcImpl.*(..))")
+	public void pt() {}
+	@Pointcut(value = "execution(* com.spring.core.session05.aop.CalcImpl.div(..))")
+	public void pt2() {}
 	
 	// 前置通知
 	// Spring EL : execution(...)
-	@Before(value = "execution(public Integer com.spring.core.session05.aop.CalcImpl.add(Integer, Integer))") // 切入點表達式
+	//@Before(value = "execution(public Integer com.spring.core.session05.aop.CalcImpl.add(Integer, Integer))") // 切入點表達式
+	//@Before(value = "execution(public Integer com.spring.core.session05.aop.CalcImpl.div(Integer, Integer))") // 切入點表達式
+	//@Before(value = "execution(public Integer com.spring.core.session05.aop.CalcImpl.*(Integer, Integer))") // 忽略方法名
+	//@Before(value = "execution(public Integer com.spring.core.session05.aop.CalcImpl.*(..))") // 忽略方法名與參數
+	//@Before(value = "execution(public Integer com.spring.core.session05.aop.*.*(..))") // 忽略類別名,方法名與參數
+	//@Before(value = "execution(* com.spring.core.session05.aop.*.*(..))") // 忽略權限,回傳值,類別名,方法名與參數
+	//@Before(value = "execution(* *(..))") // 全部攔截
+	//@Before(value = "pt()") // 透過切入點(Pointcut)方法來配置
+	@Before(value = "pt() && !pt2()") // 支援 &&, ||, !
 	public void beforeAdvice(JoinPoint joinPoint) { // JoinPoint 連接點
 		String methodName = joinPoint.getSignature().getName(); // 取得連接點的方法名稱
 		Object[] args = joinPoint.getArgs(); // 取得方法參數
