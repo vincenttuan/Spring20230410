4e4fb28f8db04b66d5054591656e7c0e44b079bb
teacher <teacher@BE-403->
2023-05-01 19:49:14 +0800

    spring

--- Spring20230410/src/com/spring/core/session05/aop_lab/Dancer.java
+++ Spring20230410/src/com/spring/core/session05/aop_lab/Dancer.java
@@ -1,5 +1,7 @@
 package com.spring.core.session05.aop_lab;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Component;
 
 // 舞者
@@ -10,7 +12,12 @@ public class Dancer implements Performance {
 	public void perform() {
 		// 業務邏輯
 		System.out.println("舞者表演開始: 跳舞, 轉圈, 翻滾...");
-		// ...
+		if(new Date().getTime() % 3 == 0) {
+			// ... 發生意外
+			throw new RuntimeException("舞者跌倒");
+		} else {
+			// ... 繼續跳
+		}
 		System.out.println("舞者表演結束");
 	}
 	
