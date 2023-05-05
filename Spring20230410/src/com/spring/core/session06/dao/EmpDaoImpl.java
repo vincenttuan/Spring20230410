365a69e93fbb3e2ce3162198c304a161c1ca2895
teacher <teacher@BE-403->
2023-05-05 18:59:20 +0800

    java

---
+++ Spring20230410/src/com/spring/core/session06/dao/EmpDaoImpl.java
@@ -0,0 +1,67 @@
+package com.spring.core.session06.dao;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.spring.core.session06.entity.Emp;
+
+@Repository
+public class EmpDaoImpl implements EmpDao {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Override
+	public List<Map<String, Object>> query() {
+		String sql = "select eid, ename, age, createtime from emp";
+		List<Map<String, Object>> emps = jdbcTemplate.queryForList(sql);
+		return emps;
+	}
+
+	@Override
+	public List<Emp> queryEmps() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Emp> getOne(Integer eid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer count() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int create(String ename, Integer age) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[] batchCreate(List<Object[]> list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Integer eid, String ename, Integer age) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Integer eid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
