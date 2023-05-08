ebcd28a5639c3b086e8ec74fc2ac17e801e3a7ac
teacher <teacher@BE-403->
2023-05-08 20:36:18 +0800

    spring

--- Spring20230410/src/com/spring/core/session07/tx/service/BookService.java
+++ Spring20230410/src/com/spring/core/session07/tx/service/BookService.java
@@ -2,5 +2,5 @@ package com.spring.core.session07.tx.service;
 
 public interface BookService {
 	void buyOne(String username, Integer bookId);
-	void buyMany(String username, Integer... bookId);
+	void buyMany(String username, Integer... bookIds);
 }
---
+++ Spring20230410/src/com/spring/core/session07/tx/service/BookServiceImpl.java
@@ -0,0 +1,31 @@
+package com.spring.core.session07.tx.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.core.session07.tx.dao.BookDao;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+	@Autowired
+	private BookDao bookDao;
+	
+	@Override
+	public void buyOne(String username, Integer bookId) {
+		// 1. 查詢書籍價格
+		int price = bookDao.getPrice(bookId);
+		// 2. 減去 Stock 庫存
+		bookDao.updateStock(bookId);
+		// 3. 修改 Wallet 餘額
+		bookDao.updateWallet(username, price);
+	}
+
+	@Override
+	public void buyMany(String username, Integer... bookIds) {
+		for(Integer bookId : bookIds) {
+			buyOne(username, bookId);
+		}	
+	}
+	
+}
