bb961de9acb771e0890d2d3e5c36e56c128bd0aa
teacher <teacher@BE-403->
2023-05-08 21:21:49 +0800

    spring

--- Spring20230410/src/com/spring/core/session07/tx/controller/BookController.java
+++ Spring20230410/src/com/spring/core/session07/tx/controller/BookController.java
@@ -3,20 +3,24 @@ package com.spring.core.session07.tx.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import com.spring.core.session07.tx.service.BookService;
+import com.spring.core.session07.tx.service.BookManyService;
+import com.spring.core.session07.tx.service.BookOneService;
 
 @Controller
 public class BookController {
 	@Autowired
-	private BookService bookService;
+	private BookOneService bookOneService;
+	
+	@Autowired
+	private BookManyService bookManyService;
 	
 	public void buyBook(String username, Integer bookId) {
-		bookService.buyOne(username, bookId);
+		bookOneService.buyOne(username, bookId);
 		System.out.println("Buy one book OK !");
 	}
 	
 	public void buyBooks(String username, Integer... bookIds) {
-		bookService.buyMany(username, bookIds);
+		bookManyService.buyMany(username, bookIds);
 		System.out.println("Buy many books OK !");
 	}
 	
