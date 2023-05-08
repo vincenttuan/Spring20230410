57c6e7116b8ad831a78be1a2aa672f722f329ea7
teacher <teacher@BE-403->
2023-05-08 20:28:16 +0800

    spring

---
+++ Spring20230410/src/com/spring/core/session07/tx/service/BookService.java
@@ -0,0 +1,6 @@
+package com.spring.core.session07.tx.service;
+
+public interface BookService {
+	void buyOne(String username, Integer bookId);
+	void buyMany(String username, Integer... bookId);
+}
