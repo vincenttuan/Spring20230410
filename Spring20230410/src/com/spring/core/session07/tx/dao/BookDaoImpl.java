0ca79ac0a2fdfe04936894d51143cb0c1030d905
teacher <teacher@BE-403->
2023-05-08 19:52:50 +0800

    spring

---
+++ Spring20230410/src/com/spring/core/session07/tx/dao/BookDaoImpl.java
@@ -0,0 +1,34 @@
+package com.spring.core.session07.tx.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookDaoImpl implements BookDao {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Override
+	public Integer getPrice(Integer bookId) {
+		// 取得書本價格
+		String sql = "select book_price from book where bood_id=?";
+		Object[] args= {bookId};
+		Integer bookPrice = jdbcTemplate.queryForObject(sql, args, Integer.class);
+		return bookPrice;
+	}
+
+	@Override
+	public Integer updateStock(Integer bookId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer updateWallet(String username, Integer price) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
