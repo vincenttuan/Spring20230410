20ac759782b06f465d6c37334d3df6840e5714be
teacher <teacher@BE-403->
2023-05-22 18:55:20 +0800

    mvc

--- SpringMVC20230512/src/main/java/spring/mvc/session09/controller/LottoController.java
+++ SpringMVC20230512/src/main/java/spring/mvc/session09/controller/LottoController.java
@@ -1,5 +1,22 @@
 package spring.mvc.session09.controller;
 
-public class LottoController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/lotto")
+public class LottoController {
+	
+	// 首頁
+	@RequestMapping("/")
+	public String index() {
+		return "session09/lotto";
+	}
+	
+	// 取得/新增一筆 lotto
+	
+	// 修改 lotto
+	
+	// 刪除 lotto
+	
 }
