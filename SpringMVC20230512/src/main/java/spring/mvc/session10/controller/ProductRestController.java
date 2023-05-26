ae0acf4862bca7551a94c3a46172c3be791d4947
teacher <teacher@BE-403->
2023-05-26 21:10:28 +0800

    ok

--- SpringMVC20230512/src/main/java/spring/mvc/session10/controller/ProductRestController.java
+++ SpringMVC20230512/src/main/java/spring/mvc/session10/controller/ProductRestController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -58,8 +59,25 @@ public class ProductRestController {
 		return "redirect:addOK";
 	}
 	
-	// 新增商品-成功
-	@GetMapping(value = "/addOK")
+	// 修改商品
+	@PutMapping("/{index}")
+	public String update(@PathVariable("index") int index, Product product, RedirectAttributes attr) {
+		// 驗證
+		if(product.getName() == null || product.getQuantity() == null || product.getPrice() == null) {
+			attr.addFlashAttribute("message", "修改資料錯誤");
+			return "redirect:error";
+		}
+		// 進行修改程序...
+		products.set(index, product);
+		// 將 product 物件資料傳遞給 /updateOK 再傳給 success.jsp 顯示, 可以防止二次 submit
+		attr.addFlashAttribute(product);
+		// 返回 addOK
+		return "redirect:updateOK";
+	}
+	
+	
+	// 新增修改商品-成功
+	@GetMapping(value = {"/addOK", "/updateOK"})
 	public String success() {
 		return "session10/rest/success";
 	}
