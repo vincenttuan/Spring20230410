208aaca43b75f43288b761085d6221f70b3ab96f
teacher <teacher@BE-403->
2023-05-29 20:41:28 +0800

    mvc

--- SpringMVC20230512/src/main/java/spring/mvc/session11/controller/UserController.java
+++ SpringMVC20230512/src/main/java/spring/mvc/session11/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import spring.mvc.session11.entity.User;
@@ -24,7 +25,7 @@ public class UserController {
 		model.addAttribute("_method", "POST");
 		model.addAttribute("submitButtonName", "新增");
 		model.addAttribute("users", users);
-		
+		/*
 		user.setName("Vincent");
 		user.setAge(18);
 		user.setBirth(new Date());
@@ -32,7 +33,14 @@ public class UserController {
 		user.setSex("男");
 		user.setInterest(new String[] {"爬山", "飛控"});
 		user.setResume("我的履歷資料:\n1:我的家庭...\n2:我喜歡...");
+		*/
 		return "session11/user";
+	}
+	
+	@PostMapping("/")
+	public String add(@ModelAttribute User user) {
+		users.add(user);
+		return "redirect:./";
 	}
 	
 }
