f596713ffb56ca487bd80a9bf630ffb42e5a0594
teacher <teacher@BE-403->
2023-05-29 19:13:35 +0800

    mvc

---
+++ SpringMVC20230512/src/main/java/spring/mvc/session11/entity/User.java
@@ -0,0 +1,67 @@
+package spring.mvc.session11.entity;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class User {
+	
+	private String name;
+	private Integer age;
+	private Date birth;
+	private String education;
+	private String sex;
+	private String[] interest;
+	private String resume;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String[] getInterest() {
+		return interest;
+	}
+	public void setInterest(String[] interest) {
+		this.interest = interest;
+	}
+	public String getResume() {
+		return resume;
+	}
+	public void setResume(String resume) {
+		this.resume = resume;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + ", birth=" + birth + ", education=" + education + ", sex=" + sex
+				+ ", interest=" + Arrays.toString(interest) + ", resume=" + resume + "]";
+	}
+	
+	
+	
+}
