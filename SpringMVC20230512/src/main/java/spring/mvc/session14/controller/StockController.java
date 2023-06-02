44ad79ccf70a89ae18560bafb11c466d0734cf61
teacher <teacher@BE-403->
2023-06-02 21:11:39 +0800

    mvc

--- SpringMVC20230512/src/main/java/spring/mvc/session14/controller/StockController.java
+++ SpringMVC20230512/src/main/java/spring/mvc/session14/controller/StockController.java
@@ -1,5 +1,37 @@
 package spring.mvc.session14.controller;
 
-public class StockController {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import spring.mvc.session14.entity.Stock;
+
+@Controller
+@RequestMapping("/session14/stock")
+public class StockController {
+	
+	private List<Stock> stocks = new CopyOnWriteArrayList<>();
+	
+	@GetMapping("/")
+	public String index(Model model, @ModelAttribute Stock stock) {
+		model.addAttribute("stocks", stocks);
+		return "session14/stock";
+	}
+	
+	@PostMapping("/")
+	public String add(Model model, @Valid Stock stock, BindingResult result) {
+		
+		stocks.add(stock);
+		return "redirect:./";
+	}
+	
 }
