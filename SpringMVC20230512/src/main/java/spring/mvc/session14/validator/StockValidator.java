e88f3fce84c9fe1399f36e6c1c10eb74a7a0a233
teacher <teacher@BE-403->
2023-06-02 21:21:58 +0800

    mvc

--- SpringMVC20230512/src/main/java/spring/mvc/session14/validator/StockValidator.java
+++ SpringMVC20230512/src/main/java/spring/mvc/session14/validator/StockValidator.java
@@ -1,5 +1,22 @@
 package spring.mvc.session14.validator;
 
-public class StockValidator {
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class StockValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
