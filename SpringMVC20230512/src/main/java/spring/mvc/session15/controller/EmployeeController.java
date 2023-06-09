7148c7b13626015579807111c3eb8803818fae02
teacher <teacher@BE-403->
2023-06-09 20:01:14 +0800

    lab

--- SpringMVC20230512/src/main/java/spring/mvc/session15/controller/EmployeeController.java
+++ SpringMVC20230512/src/main/java/spring/mvc/session15/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import spring.mvc.session15.entity.Employee;
@@ -17,13 +18,25 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeDao employeeDao;
 	
-	@GetMapping
+	@GetMapping("/")
 	public String index(@ModelAttribute Employee employee, Model model) {
 		model.addAttribute("_method", "POST");
 		model.addAttribute("employees", employeeDao.query());
 		model.addAttribute("pageCount", getPageCount());
 		return "session15/employee";
 	}
+	
+	@GetMapping("/page/{num}")
+	public String page(@PathVariable("num") int num, @ModelAttribute Employee employee, Model model) {
+		if(num < 0) {
+			return "redirect:../";
+		}
+		model.addAttribute("_method", "POST");
+		model.addAttribute("employees", employeeDao.queryPage(num));
+		model.addAttribute("pageCount", getPageCount());
+		return "session15/employee";
+	}
+	
 	
 	// 取得總頁數
 	private int getPageCount() {
