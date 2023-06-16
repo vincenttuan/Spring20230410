bb6db5aaf5b8d418aff188307bc124c92b783de4
teacher <teacher@BE-403->
2023-06-16 19:20:59 +0800

    mvc

--- SpringMVC20230512/src/main/java/spring/mvc/session17/controller/DivisionController.java
+++ SpringMVC20230512/src/main/java/spring/mvc/session17/controller/DivisionController.java
@@ -1,11 +1,16 @@
 package spring.mvc.session17.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import spring.mvc.session17.entity.Division;
 
@@ -25,4 +30,13 @@ public class DivisionController {
 		return "session17/division";
 	}
 	
+	// 捕獲使用者輸入資料格式不正確的例外
+	@ExceptionHandler({BindException.class})
+	@ResponseBody
+	public String catchException(Exception ex, Model model, HttpServletRequest request) {
+		String referer = request.getHeader("Referer");
+		model.addAttribute("referer", referer);
+		model.addAttribute("ex", ex);
+		return referer + "\n" + ex;
+	}
 }
