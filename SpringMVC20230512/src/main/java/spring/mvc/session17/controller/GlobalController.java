b9bc25ef59d95ada78f0c3e48392726382b713c8
teacher <teacher@BE-403->
2023-06-16 20:52:13 +0800

    mvc

---
+++ SpringMVC20230512/src/main/java/spring/mvc/session17/controller/GlobalController.java
@@ -0,0 +1,26 @@
+package spring.mvc.session17.controller;
+
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+//在Spring裡，我們可以使用@ControllerAdvice來聲明一些全局性的東西
+//@ExceptionHandler 註解用於全局異常的處理。
+//@ModelAttribute 可用於全局使用特定資料時使用
+//@InitBinder 初始化繫結器，用於資料繫結、設定資料轉換器等
+@ControllerAdvice
+public class GlobalController {
+	
+	// 全局例外 
+	@ExceptionHandler({RuntimeException.class, SQLException.class})
+	public String globalException(Exception ex, Model model, HttpServletRequest request) {
+		String referer = request.getHeader("Referer");
+		model.addAttribute("referer", referer);
+		model.addAttribute("ex", "@ControllerAdvice 全局例外:" + ex);
+		return "session17/error";
+	}
+}
