81aa440897db574bab35de597cb13e0b25457df2
teacher <teacher@BE-403->
2023-06-16 21:07:28 +0800

    mvc

---
+++ SpringMVC20230512/src/main/java/spring/mvc/session17/entity/Book.java
@@ -0,0 +1,26 @@
+package spring.mvc.session17.entity;
+
+public class Book {
+	private String name;
+	private Long price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getPrice() {
+		return price;
+	}
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book [name=" + name + ", price=" + price + "]";
+	}
+	
+	
+}
