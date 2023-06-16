4510af623f70456c26c095787e756eb1f871bb43
teacher <teacher@BE-403->
2023-06-16 21:00:14 +0800

    mvc

--- SpringMVC20230512/src/main/java/spring/mvc/session17/controller/GlobalController.java
+++ SpringMVC20230512/src/main/java/spring/mvc/session17/controller/GlobalController.java
@@ -1,12 +1,15 @@
 package spring.mvc.session17.controller;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 //在Spring裡，我們可以使用@ControllerAdvice來聲明一些全局性的東西
 //@ExceptionHandler 註解用於全局異常的處理。
@@ -22,5 +25,14 @@ public class GlobalController {
 		model.addAttribute("referer", referer);
 		model.addAttribute("ex", "@ControllerAdvice 全局例外:" + ex);
 		return "session17/error";
+	}
+	
+	// 全局 Model
+	@ModelAttribute(name = "globalData")
+	public Map<String, Object> globalModelData() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("copyright", "ABC 股份有限公司版權所有");
+		map.put("address", "台北市忠孝東路四段169號4樓");
+		return map;
 	}
 }
