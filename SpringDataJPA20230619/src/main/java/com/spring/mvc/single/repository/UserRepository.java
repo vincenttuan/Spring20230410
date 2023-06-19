c533d3d456560d9456e1c63fd9fe20ccb25873de
teacher <teacher@BE-403->
2023-06-19 19:51:02 +0800

    jpa

--- SpringDataJPA20230619/src/main/java/com/spring/mvc/single/repository/UserRepository.java
+++ SpringDataJPA20230619/src/main/java/com/spring/mvc/single/repository/UserRepository.java
@@ -1,5 +1,16 @@
 package com.spring.mvc.single.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.mvc.single.entity.User;
+
+/*
+ * 方法命名規則
+ * 1. 查詢方法以 find | read | get 開頭
+ * 2. 涉及條件查詢時，條件的屬性（首字母大寫）用條件關鍵字（例如：And、Or）連結
+ * */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>  {
 
 }
