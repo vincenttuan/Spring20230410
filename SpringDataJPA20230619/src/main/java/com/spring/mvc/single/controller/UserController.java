0feb95aad6563f66d3ab24cf1faf691abd4b10de
teacher <teacher@BE-403->
2023-06-19 20:42:35 +0800

    jpa

--- SpringDataJPA20230619/src/main/java/com/spring/mvc/single/controller/UserController.java
+++ SpringDataJPA20230619/src/main/java/com/spring/mvc/single/controller/UserController.java
@@ -1,5 +1,29 @@
 package com.spring.mvc.single.controller;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.mvc.single.entity.User;
+import com.spring.mvc.single.repository.UserRepository;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping
+	public String index(@ModelAttribute User user, Model model) {
+		List<User> users = userRepository.findAll();
+		model.addAttribute("_method", "POST");
+		model.addAttribute("users", users);
+		return "user/index";
+	}
 }
